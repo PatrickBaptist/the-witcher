9ba7c6bdd0649450e53e4cec5a9653a8b66e6eb1
Patrick Baptista <133401579+PatrickBaptist@users.noreply.github.com>
2023-12-01 15:29:10 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { UserStore } from './contexts/userContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserStore>
     <App />
-    </UserStore>
   </React.StrictMode>
 );
